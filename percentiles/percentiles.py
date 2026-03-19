0be73034aaa273247e4c3a4fd164d87c660e7952
cardioio <41829554+cardioio@users.noreply.github.com>
2026-03-19 14:42:49 +0800

    Add solution for percentiles

---
+++ percentiles/percentiles.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+def percentiles(x, q):
+    """
+    Compute percentiles using linear interpolation.
+    """
+    # Write code here
+    x = np.array(x)
+    x = np.sort(x)
+    return np.percentile(x,q,method='linear')
+    
+    
